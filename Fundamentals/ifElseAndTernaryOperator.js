47932c5d179385b28ded8781e0201ef3524e1062
Armyant98 <Armyant98@users.noreply.github.com>
2023-04-02 11:53:02 -0400

    added codewars solution

--- Fundamentals/ifElseAndTernaryOperator.js
+++ Fundamentals/ifElseAndTernaryOperator.js
@@ -18,3 +18,11 @@ function saleHotdogs(n){
     return n < 5 ? n * 100 : n >= 5 && n < 10 ? n * 95 : n * 90 
   }
 
+
+  //interestion option
+
+//   function saleHotdogs(n){
+//     return n*(n<5?100:n<10?95:90);
+//   }
+
+//factors the n * and simply ternary for the numer its mulitiplying against
