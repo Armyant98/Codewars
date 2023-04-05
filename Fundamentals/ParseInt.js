91b1e69ed7a06abe46373d27ac2745888c034576
Armyant98 <Armyant98@users.noreply.github.com>
2023-04-05 16:39:46 -0400

    codewars parseInt Problem

---
+++ Fundamentals/ParseInt.js
@@ -0,0 +1,36 @@
+// DESCRIPTION:
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the 
+// format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the 
+// championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+
+
+const points=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
+
+// function points(games) {
+//   return games.reduce((output,current)=>{        going to reduce or add it all together the accumulator getting the result added to it and the current being each game being passed in
+//     let x = parseInt(current[0]);                turns the 0 index of the total game (for each game) to a number 
+//     let y = parseInt(current[2]);                2 because its parsing the third thing to a number so we jump over the :
+//     let value= x>y ? 3 : x===y ? 1 : 0;          the comparison
+//     return output+value;                         output + value to the accumulator
+//   },0)                                           start at 0
+// }
