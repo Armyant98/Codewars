882ae90b6742473dd9942d2af08e0a844e5f7a67
Armyant98 <102646883+Armyant98@users.noreply.github.com>
2023-04-17 19:53:23 -0400

    created 7kata folder and added challenge

---
+++ Codewars7/binaryAddition.js
@@ -0,0 +1,15 @@
+// Implement a function that adds two numbers together and returns their sum in binary. 
+// The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+
+addBinary = (a,b) => (a+b).toString(2)
+
+
+//to string has a property known as radix, which allows the base changes for num to string. Converts this num to a string at the base of 2 aka binary
