046ab527b23369eea822d1df8355cdd47358668d
Armyant98 <102646883+Armyant98@users.noreply.github.com>
2023-06-02 23:44:38 -0400

    added coding challenge

---
+++ Fundamentals/consecutiveStrings.js
@@ -0,0 +1,51 @@
+// You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+// Examples:
+// strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+// Concatenate the consecutive strings of strarr by 2, we get:
+
+// treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so 
+// longest_consec(strarr, 2) should return "folingtrashy".
+
+// In the same way:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+
+function longestConsec(arr, k) {
+    //return '' if  length of the string array, if n = 0 or k > n or k <= 0
+    if(arr.length === 0 || k > arr.length || k <= 0){
+      return ''
+    }
+  
+    let longestString = ''
+     let start = 0
+    // loop through array to test for each possibnility, loop through if <= length - k to avoid unneccessary loops 
+    //since extra wil run off array
+    for (let i =0; i <= arr.length - k; i++){
+     
+      
+     
+        let concatenate = arr.slice(start, start + k).join('')
+        start += 1
+        
+        if(concatenate.length > longestString.length){
+          longestString = concatenate
+        }
+      
+      
+      
+      
+    }
+    
+    // concatonate string and comapare with last string length keep whichever is bigger
+  return longestString
+  
+}
