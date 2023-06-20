e17c174f310ff9804ada8bfddbee395a2dcebb7c
Armyant98 <102646883+Armyant98@users.noreply.github.com>
2023-06-20 00:10:14 -0400

    codewars challenge

---
+++ Codewars 6+/giveMeADiamond.js
@@ -0,0 +1,79 @@
+// Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+
+// Task
+// You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with 
+// a newline character (\n).
+
+// Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+// Examples
+// A size 3 diamond:
+
+//  *
+// ***
+//  *
+// ...which would appear as a string of " *\n***\n *\n"
+
+// A size 5 diamond:
+
+//   *
+//  ***
+// *****
+//  ***
+//   *
+// ...that is:
+
+// "  *\n ***\n*****\n ***\n  *\n"
+
+
+function diamond(n){
+    if(n%2 === 0 || n < 1){
+      return null
+    }
+    
+    if(n === 1){
+      return '*\n'
+    }
+    
+    let spaces = Math.floor(n/2) + 1
+    
+    let finalString = ''
+    
+    for(let i = 0; i < n; i+= 2){
+      if(i%2 === 0){
+        spaces-= 1
+      }else{
+        
+      }
+        
+      if(i+1 === n){
+        spaces = 0
+      }
+      finalString += ' '.repeat(spaces) + '*'.repeat(i+1) + '\n' 
+    }
+    
+    console.log(finalString) // up to this part is good 
+    
+    let reverse = finalString.split('\n').reverse()
+    reverse.shift()
+    reverse.shift()
+    reverse = reverse.map(str => str + '\n').join('')
+    
+    finalString += reverse
+  
+    return finalString
+  }
+
+
+
+  function diamond (n) {
+    if (n <= 0 || n % 2 === 0) return null
+    str = ''
+    for (let i = 0; i < n; i++) { 
+      let len = Math.abs((n-2*i-1)/2)
+      str += ' '.repeat(len)
+      str += '*'.repeat(n-2*len)
+      str += '\n'
+    }
+    return str
+  }
